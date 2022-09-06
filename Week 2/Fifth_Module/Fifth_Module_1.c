4f00cc523b3c53d8f844e9a895026258f1eed386
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-09-07 05:59:03 +0600

    Second_Week

---
+++ Week 2/Fifth_Module/Fifth_Module_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    for (int i=5; i<=10; i++){
+        printf("Hello world\n");
+    }
+    return 0;
+}
+
+/*
+for (starting code; continue condition; repeatation) {
+    statements;
+}
+
+starting code;
+
+loop:
+    continue code
+        true:
+            statement
+            repeation
+        false:
+            stop
+*/
