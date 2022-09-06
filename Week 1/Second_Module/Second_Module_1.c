565abf3de64ddf3d36e325cf609916facde99692
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-09-07 05:49:06 +0600

    First_Week

--- Week 1/Second_Module/Second_Module_1.c
+++ Week 1/Second_Module/Second_Module_1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int price = 10;
+    printf("price = %d\n", price);
+
+    float marks = 33.9;
+    printf("marks = %f\n", marks);
+
+    double temparature = 10.5;
+    printf("temparature = %lf\n", temparature);
+
+    char character = 'a';
+    printf("character = %c\n", character);
+
+    return 0;
+}
