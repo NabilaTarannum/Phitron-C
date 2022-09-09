009ccb5951dc86b92958b2e06edff72d5bb2dd10
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-09-09 23:58:35 +0600

    Second_Week

---
+++ Week 2/Sixth_Module/Sixth_Module_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d",  &t);
+
+    for (int cs= 0; cs < t; cs++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("Case %d: %d\n", cs + 1, a + b);
+    }
+    return 0; 
+}
